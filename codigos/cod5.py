4096fc4f6e1674e81f5ee11f289a39e8fa510b1d
Luis Hernan <ramirezluishernan5@gmail.com>
2025-03-11 17:30:09 -0600

    add

--- codigos/cod5.py
+++ codigos/cod5.py
@@ -7,30 +7,26 @@ class Usuario():
         self.nombre = nombre
         self.apellidos = apellidos
         self.edad = edad
-        self.direccion = direccion
         self.telefono = telefono
 
-    def iniciar_sesion(self):
+        self.direccion = "Sin direccion"
+
+    def iniciar_sesion():
         print("El usuario ha iniciado sesión.")
     def cerrar_sesion(self):
         print("El usuario ha cerrado sesión.")
     def cambiar_nombre_perfil(self):
         print("Se cambió el nombre.")
-#
-#usuario1 = Usuario("Luis Hernán", "Ramírez", 18, "55555555")
-#print(usuario1.nombre)
-#print(usuario1.direccion)
-## Reasignaciones:
-#usuario1.nombre = "Luis"
-#usuario1.hora_ultimo_inicio = "10/03/25"
-#
-## Probamos los atributos:
-#print(usuario1.nombre)
-#print(usuario1.hora_ultimo_inicio)
-#
-#usuario1.iniciar_sesion()
 
+usuario1 = Usuario("Luis Hernán", "Ramírez", 18, "55555555")
+print(usuario1.nombre)
+print(usuario1.direccion)
+# Reasignaciones:
+usuario1.nombre = "Luis"
+usuario1.hora_ultimo_inicio = "10/03/25"
 
-usuario_2 = Usuario("Adrian", "Fernandez", 32, "Sin direccion", "9999999")
+# Probamos los atributos:
+print(usuario1.nombre)
+print(usuario1.hora_ultimo_inicio)
 
-Usuario.iniciar_sesion(usuario_2)
+usuario1.iniciar_sesion()
