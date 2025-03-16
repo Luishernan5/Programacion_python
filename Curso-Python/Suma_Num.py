d5b03f8852b0991c559e0c7bf36852d2b3070923
Luis Hernan <ramirezluishernan5@gmail.com>
2025-03-15 18:27:58 -0600

    add

---
+++ Curso-Python/Suma_Num.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio 2: Suma de dos números
+y mostrar resultado.
+"""
+numero1 = float(input("Ingresa un numero: "))
+numero2 = float(input("Ingresa el otro numero: "))
+
+suma = numero1 + numero2
