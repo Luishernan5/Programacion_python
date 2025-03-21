02b0ee07461b2395eeed9bf25710c828bec9996c
Luis Hernan <ramirezluishernan5@gmail.com>
2025-03-21 13:17:03 -0600

    add

--- Curso-Python/Reemplazo_Caracter.py
+++ Curso-Python/Reemplazo_Caracter.py
@@ -6,7 +6,7 @@ var = True
 while var:
     cadena = input("Ingresa una cadena (0 para salir): ")
     if cadena != "0":
-        nueva_cadena = cadena.replace("o", "x")
+        nueva_cadena = cadena.replace("o", "x","y", "s")
         print(f"La nueva cadena de {cadena} es: ", nueva_cadena)
     else:
         print("Saliendo....")
