721afb4fd1447a3cdcd975263fb9988d0365e928
Luis Hernan <ramirezluishernan5@gmail.com>
2025-03-23 19:42:46 -0600

    add

--- Curso-Python/Invertir_Cadena.py
+++ Curso-Python/Invertir_Cadena.py
@@ -6,7 +6,7 @@ while var:
     cadena = input("ingresa una cadena (0 para salir): ")
     if cadena != "0":
         invertir = cadena[::-1]
-        print(f"La cadena inversa de {cadena} es: ", invertir)
+        print(f"La cadena inversa de {cadena} es:", invertir)
     else:
         print("Saliendo....")
         break
