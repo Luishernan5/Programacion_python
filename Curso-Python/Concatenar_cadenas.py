3d7b14fda8b770876a979f6e5f0dc8407df3af3b
Luis Hernan <ramirezluishernan5@gmail.com>
2025-03-23 19:42:24 -0600

    add

--- Curso-Python/Concatenar_cadenas.py
+++ Curso-Python/Concatenar_cadenas.py
@@ -5,6 +5,6 @@ Ejercicio 5: Concatenar dos cadenas de texto.
 cadena1 = "Hola."
 cadena2 = "¿Como estas?"
 
-concatenacion = cadena1 + " " + cadena2
+concatenacion = cadena1 + "  " + cadena2
 
 print("La cadena es: ", concatenacion)
