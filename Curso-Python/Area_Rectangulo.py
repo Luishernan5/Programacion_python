df9707d1a63a56cbeccdcfe8063b6e8df137901c
Luis Hernan <ramirezluishernan5@gmail.com>
2025-03-23 19:41:44 -0600

    add

--- Curso-Python/Area_Rectangulo.py
+++ Curso-Python/Area_Rectangulo.py
@@ -3,7 +3,7 @@ Ejercicio 15: Calcula el area de un rectangulo
 donde el usuario ingresa la base y la altura.
 """
 
-base = int(input("Ingresa la longitud de la base: "))
+base = int(input("Ingresa la longitud de la base:"))
 altura = int(input("Ingresa la longitud de la altura: "))
 
 area = base * altura
