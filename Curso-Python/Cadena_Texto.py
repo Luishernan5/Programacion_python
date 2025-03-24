c04abb637718772fbc744765abf9a6538dbbc6ea
Luis Hernan <ramirezluishernan5@gmail.com>
2025-03-23 19:42:02 -0600

    add

--- Curso-Python/Cadena_Texto.py
+++ Curso-Python/Cadena_Texto.py
@@ -3,7 +3,7 @@ Ejercicio 8: Crea una cadena de texto y
 muestra su longitud.
 """
 
-cadena = input("Ingresa una cadena de texto: ")
+cadena = input("Ingresa una cadena de texto:")
 longitud = len(cadena)
 
 print("La longitud es: ", longitud)
